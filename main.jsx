eb45e027a9bc3e206a171f99b9a7d9f995f533c8
skasle <sam@eightbyzero.com>
2026-04-15 15:41:54 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Issue001 from "./Issue001";
+import Issue002 from "./Issue002";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const path = window.location.pathname;
+
+let Component = Issue002; // default to latest
+if (path.includes("001")) Component = Issue001;
+if (path.includes("002")) Component = Issue002;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Component />
+  </React.StrictMode>
+);
